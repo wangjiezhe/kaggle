17344160248293ebd32b806358e5109ad67c4bb8
wangjiezhe <wangjiezhe@gmail.com>
2024-01-03 17:31:20 +0800

    Classify Leaves: use augmentation and other convnets

--- classify_leaves/data.py
+++ classify_leaves/data.py
@@ -11,34 +11,46 @@ from torchvision.transforms import v2
 
 class LeavesData(pl.LightningDataModule):
     def __init__(
-        self, batch_size=64, num_workers=8, aug=None, train_images_dir="./data/images_train", split=True
+        self,
+        batch_size=64,
+        num_workers=8,
+        aug=None,
+        train_images_dir="./data/images_train",
+        split=True,
+        normalize=True,
     ):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters(ignore=["aug"])
         self.train_images_dir = train_images_dir
         self.predict_images_dir = "./data"
         self.predict_csv = "./data/test.csv"
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.split = split
-        self.trans_predict = v2.Compose([v2.PILToTensor(), v2.ToDtype(torch.float32, scale=True)])
-        if aug:
-            self.trans_train = v2.Compose([aug, self.trans_predict])
-        else:
-            self.trans_train = self.trans_predict
+        trans = v2.Compose([v2.PILToTensor(), v2.ToDtype(torch.float32, scale=True)])
+        self.trans_predict = (
+            v2.Compose([trans, v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+            if normalize
+            else trans
+        )
+        self.trans_train = v2.Compose([aug, self.trans_predict]) if aug else self.trans_predict
+        self.label_map = {
+            v: k for k, v in ImageFolder(self.train_images_dir, self.trans_predict).class_to_idx.items()
+        }
 
     def setup(self, stage=None):
-        dataset = ImageFolder(self.train_images_dir, self.trans_train)
-        self.label_map = {v: k for k, v in dataset.class_to_idx.items()}
         if stage == "fit" or stage is None:
+            train_dataset = ImageFolder(self.train_images_dir, self.trans_train)
             if self.split:
-                self.train_data, self.val_data = random_split(dataset, [0.9, 0.1])
+                self.train_data, self.val_data = random_split(train_dataset, [0.9, 0.1])
             else:
-                self.train_data = dataset
-                _, self.val_data = random_split(dataset, [0.9, 0.1])
-        if stage == "test" or stage is None:
-            self.test_data = dataset
-        if stage == "predict" or stage is None:
+                self.train_data = train_dataset
+                _, self.val_data = random_split(
+                    ImageFolder(self.train_images_dir, self.trans_predict), [0.9, 0.1]
+                )
+        if stage == "test":
+            self.test_data = ImageFolder(self.train_images_dir, self.trans_predict)
+        if stage == "predict":
             self.predict_images = pd.read_csv(self.predict_csv)
             self.predict_data = TensorDataset(
                 torch.stack([self.image_to_tensor(f) for f in self.predict_images["image"].tolist()])
