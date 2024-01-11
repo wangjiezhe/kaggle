09c5d97736fd2a5496212d8a3a10af7f95646a1e
wangjiezhe <wangjiezhe@gmail.com>
2024-01-12 00:11:33 +0800

    Dog Breed Identification: finish

---
+++ dog_breed_identification/model.py
@@ -0,0 +1,259 @@
+from typing import Optional
+
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torchmetrics import Accuracy, ConfusionMatrix
+from torchvision import models
+from utils import init_cnn, NUM_CLASSES
+
+__all__ = ["Classifier", "DefinedNet", "PretrainedNet", "ResNet", "RegNet", "ConvNeXt"]
+
+
+class Classifier(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+        self.train_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro")
+        self.val_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro")
+        self.test_top1_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro")
+        self.test_top2_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro", top_k=2)
+        self.test_top5_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro", top_k=5)
+        self.test_top9_acc = Accuracy("multiclass", num_classes=NUM_CLASSES, average="micro", top_k=9)
+        self.confusion_matrix = ConfusionMatrix("multiclass", num_classes=NUM_CLASSES)
+
+    def on_train_epoch_start(self):
+        self.train_acc.reset()
+
+    def on_validation_epoch_start(self):
+        self.val_acc.reset()
+
+    def on_test_epoch_start(self):
+        self.test_top1_acc.reset()
+        self.test_top2_acc.reset()
+        self.test_top5_acc.reset()
+        self.test_top9_acc.reset()
+        self.confusion_matrix.reset()
+
+    def training_step(self, batch):
+        features, labels = batch
+        preds = self(features)
+        loss = F.cross_entropy(preds, labels)
+        metric = {"train_loss": loss}
+        if self.trainer.datamodule.mix is None:
+            with torch.no_grad():
+                metric["train_acc"] = self.train_acc(preds, labels)
+        self.log_dict(metric, prog_bar=True)
+        return loss
+
+    def on_train_epoch_end(self):
+        if self.trainer.datamodule.mix is None:
+            self.log_dict({"train_epoch_acc": self.train_acc.compute()})
+
+    def validation_step(self, batch):
+        features, labels = batch
+        preds = self(features)
+        loss = F.cross_entropy(preds, labels)
+        acc = self.val_acc(preds, labels)
+        self.log_dict({"val_loss": loss, "val_acc": acc}, prog_bar=True)
+        return loss
+
+    def test_step(self, batch):
+        features, labels = batch
+        preds = self(features)
+        loss = F.cross_entropy(preds, labels)
+        top1_acc = self.test_top1_acc(preds, labels)
+        top2_acc = self.test_top2_acc(preds, labels)
+        top5_acc = self.test_top5_acc(preds, labels)
+        top9_acc = self.test_top9_acc(preds, labels)
+        self.confusion_matrix(preds, labels)
+        self.log_dict(
+            {
+                "test_loss": loss,
+                "test_top1_acc": top1_acc,
+                "test_top2_acc": top2_acc,
+                "test_top5_acc": top5_acc,
+                "test_top9_acc": top9_acc,
+            }
+        )
+        return loss
+
+    def on_test_epoch_end(self):
+        self.logger.log_hyperparams(
+            self.hparams,
+            {
+                "hp/train_acc": self.train_acc.compute(),
+                "hp/val_acc": self.val_acc.compute(),
+                "hp/test_acc": self.test_top1_acc.compute(),
+            },
+        )
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        features = batch
+        if dataloader_idx == 0:
+            preds = self(features)
+        elif dataloader_idx == 1:
+            _, _, c, h, w = features.shape
+            preds = self(features.view(-1, c, h, w))
+        return preds
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters())
+
+
+class DefinedNet(Classifier):
+    def __init__(self, model: str, pretrained=False, use_timm=False):
+        super().__init__()
+        self.save_hyperparameters()
+        if use_timm:
+            import timm
+
+            available_models = timm.list_models()
+            if model not in available_models:
+                raise ValueError(f"{model} should be one of {available_models}.")
+            self.backbone = timm.create_model(model, pretrained=pretrained)
+        else:
+            available_models = models.list_models()
+            if model not in available_models:
+                raise ValueError(f"{model} should be one of {available_models}.")
+            weights = "DEFAULT" if pretrained else None
+            self.backbone = models.get_model(model, weights=weights)
+        self.classifier = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.LazyLinear(256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.LazyLinear(NUM_CLASSES),
+        )
+
+    def forward(self, X):
+        y = self.backbone(X)
+        y = self.classifier(y)
+        return y
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(filter(lambda p: p.requires_grad, self.parameters()))
+
+
+class PretrainedNet(Classifier):
+    def __init__(self, model: str, pretrained=False):
+        super().__init__()
+        self.save_hyperparameters()
+
+        available_models = models.list_models()
+        if model not in available_models:
+            raise ValueError(f"{model} should be one of {available_models}.")
+        weights = "DEFAULT" if pretrained else None
+        self.net = models.get_model(model, weights=weights)
+        layers = [k for k, _ in self.net.named_children()]
+        match layers[-1]:
+            case "fc":
+                # ResNet, RegNet, ResNeXt, Inception, GoogleNet, ShuffleNet
+                assert isinstance(self.net.fc, nn.Linear)
+                self.net.fc = nn.LazyLinear(NUM_CLASSES)
+            case "classifier":
+                if isinstance(self.net.classifier, nn.Linear):
+                    # DenseNet
+                    self.net.classifier = nn.LazyLinear(NUM_CLASSES)
+                elif isinstance(self.net.classifier[-1], nn.Linear):
+                    # VGG, ConvNeXt[2022], EfficientNet, AlexNet, MNASNet, MobileNet
+                    self.net.classifier[-1] = nn.LazyLinear(NUM_CLASSES)
+                else:
+                    # SqueezeNet
+                    assert layers == ["features", "classifier"]
+                    assert len(self.net.classifier) == 4
+                    self.net.classifier[1] = nn.LazyConv2d(NUM_CLASSES, kernel_size=(1, 1), stride=(1, 1))
+            case "head":
+                # Swin
+                assert isinstance(self.net.heads, nn.Linear)
+                self.net.head = nn.LazyLinear(NUM_CLASSES)
+            case "heads":
+                # ViT
+                assert isinstance(self.net.heads, nn.Linear)
+                self.net.heads = nn.LazyLinear(NUM_CLASSES)
+            case _:
+                raise ValueError(f"{model} cannot be converted")
+
+    def forward(self, X):
+        return self.net(X)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(filter(lambda p: p.requires_grad, self.parameters()))
+
+
+class ResNet(Classifier):
+    def __init__(self, model: str, pretrained=False):
+        super().__init__()
+        self.save_hyperparameters()
+        available_models = models.list_models(include="resnet*")
+        if model not in available_models:
+            raise ValueError(f"{model} should be one of {available_models}.")
+        weights = "DEFAULT" if pretrained else None
+        self.net = models.get_model(model, weights=weights)
+        in_features = self.net.fc.in_features
+        self.net.fc = nn.Linear(in_features, NUM_CLASSES)
+        self.net.fc.apply(init_cnn)
+
+    def forward(self, X):
+        return self.net(X)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(filter(lambda p: p.requires_grad, self.parameters()))
+
+
+class RegNet(Classifier):
+    def __init__(
+        self, model: str, pretrained=False, cosine_t_max: Optional[int] = None, verbose=False, **kwargs
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.cosine_t_max = cosine_t_max
+        self.pretrained = pretrained
+        self.verbose = verbose
+        available_models = models.list_models(include="regnet*")
+        if model not in available_models:
+            raise ValueError(f"{model} should be one of {available_models}.")
+        if self.pretrained:
+            self.net = models.get_model(model, weights="DEFAULT", **kwargs)
+            in_features = self.net.fc.in_features
+            self.net.fc = nn.Linear(in_features, NUM_CLASSES)
+        else:
+            self.net = models.get_model(model, num_classes=NUM_CLASSES, **kwargs)
+        self.net.fc.apply(init_cnn)
+
+    def forward(self, X):
+        return self.net(X)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, self.parameters()))
+        if self.cosine_t_max:
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(
+                    optimizer, self.cosine_t_max, eta_min=0.00001, verbose=self.verbose
+                ),
+            }
+        else:
+            return optimizer
+
+
+class ConvNeXt(Classifier):
+    def __init__(self, model: str, pretrained=False):
+        super().__init__()
+        self.save_hyperparameters()
+        available_models = models.list_models(include="convnext*")
+        if model not in available_models:
+            raise ValueError(f"{model} should be one of {available_models}.")
+        weights = "DEFAULT" if pretrained else None
+        self.net = models.get_model(model, weights=weights)
+        in_features = self.net.classifier[-1].in_features
+        self.net.classifier[-1] = nn.Linear(in_features, NUM_CLASSES)
+        self.net.classifier.apply(init_cnn)
+
+    def forward(self, X):
+        return self.net(X)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(filter(lambda p: p.requires_grad, self.parameters()))
---
+++ dog_breed_identification/train.py
@@ -0,0 +1,512 @@
+#!/usr/bin/python3
+
+import re
+from typing import Callable, List, Optional, Union
+
+import pytorch_lightning as pl
+import torch
+from callback import *
+from data import *
+from model import *
+from pytorch_lightning.callbacks import BackboneFinetuning, Callback
+from pytorch_lightning.loggers import TensorBoardLogger
+from termcolor import colored
+from torchvision.transforms import v2
+from utils import *
+
+torch.set_float32_matmul_precision("medium")
+
+
+def train(
+    model: Union[pl.LightningModule, str],
+    epoch: int,
+    pretrained=False,
+    best_model=True,
+    best_model_dirpath: Optional[str] = None,
+    callbacks: Optional[Union[List[Callback], Callback]] = None,
+    logger_version: Optional[Union[int, str, bool]] = None,
+    checkpoint_path: Optional[str] = None,
+    weights_path: Optional[str] = None,
+    save_checkpoint_path: Optional[str] = None,
+    save_weights_path: Optional[str] = None,
+    data=None,
+    batch_size=128,
+    num_workers=8,
+    resize=224,
+    aug: Optional[Callable] = None,
+    split=True,
+    split_random_seed=42,
+    five_crop=False,
+    mix: Optional[Callable] = None,
+    test=True,
+    predict=True,
+    **kwargs,
+):
+    if isinstance(model, str):
+        model = DefinedNet(model, pretrained=pretrained)
+
+    if checkpoint_path is not None:
+        state = torch.load(checkpoint_path)
+        model.load_state_dict(state["state_dict"], strict=False)
+    elif weights_path is not None:
+        state = torch.load(weights_path)
+        assert "state_dict" not in state.keys()
+        model.load_state_dict(state, strict=False)
+
+    if data is None:
+        data = DogData(
+            batch_size=batch_size,
+            num_workers=num_workers,
+            resize=resize,
+            aug=aug,
+            split=split,
+            split_random_seed=split_random_seed,
+            five_crop=five_crop,
+            mix=mix,
+        )
+    logger = TensorBoardLogger(".", default_hp_metric=False, version=logger_version)
+
+    model_name = model.hparams.get("model", model.__class__.__name__)
+    if model_name:
+        print(colored(f"Start training {model_name} in {epoch} epoches...", "green"))
+    ck = [save_last(model_name, dirpath=best_model_dirpath)]
+    if test:
+        ck += [ConfMatTopK(20)]
+    if predict:
+        ck += [Submission()]
+    if best_model:
+        best_val_acc = save_best_val_acc(model_name, dirpath=best_model_dirpath)
+        ck += [
+            save_best_train_loss(model_name, dirpath=best_model_dirpath),
+            save_best_val_loss(model_name, dirpath=best_model_dirpath),
+            best_val_acc,
+        ]
+    if callbacks:
+        if isinstance(callbacks, Callback):
+            callbacks = [callbacks]
+        ck += callbacks
+
+    trainer = pl.Trainer(max_epochs=epoch, logger=logger, callbacks=ck, **kwargs)
+    trainer.fit(model, data)
+    if test:
+        trainer.test(model, data)
+    if save_checkpoint_path is not None:
+        trainer.save_checkpoint(save_checkpoint_path)
+    if save_weights_path is not None:
+        torch.save(model.state_dict(), save_weights_path)
+
+    if predict:
+        trainer.predict(model, data)
+        if best_model:
+            best_val_acc_epoch = int(re.findall("epoch=(\d+)", best_val_acc.best_model_path)[0])
+            if best_val_acc_epoch < trainer.current_epoch - 1:
+                trainer.predict(model, data, ckpt_path=best_val_acc.best_model_path)
+
+
+def predict(
+    model, data=None, batch_size=128, five_crop=False, checkpoint_path=None, weights_path=None, write=True
+):
+    if isinstance(model, str):
+        model = DefinedNet(model)
+    if checkpoint_path is not None:
+        state = torch.load(checkpoint_path)
+        model.load_state_dict(state["state_dict"], strict=False)
+    elif weights_path is not None:
+        state = torch.load(weights_path)
+        assert "state_dict" not in state.keys()
+        model.load_state_dict(state, strict=False)
+    if data is None:
+        data = DogData(batch_size=batch_size, five_crop=five_crop)
+    callbacks = [Submission()] if write else None
+    trainer = pl.Trainer(max_epochs=0, logger=False, callbacks=callbacks)
+    return trainer.predict(model, data)
+
+
+def train_resnet18_1():
+    model = ResNet("resnet18", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(30),
+        logger_version="ResNet18_freeze_30",
+        predict=False,
+    )
+
+
+def train_resnet18_2():
+    model = ResNet("resnet18", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet18_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_resnet18_3():
+    model = ResNet("resnet18", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet18_mix_freeze_5_25",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_resnet18_4():
+    model = ResNet("resnet18", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet18_mix_freeze_5_25_autoaug",
+        mix=cutmix_or_mixup,
+        aug=v2.AutoAugment(),
+        predict=False,
+    )
+
+
+def train_resnet34_1():
+    model = ResNet("resnet34", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet34_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_resnet50_1():
+    model = ResNet("resnet50", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet50_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_resnet101_1():
+    model = ResNet("resnet101", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet101_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_resnet152_1():
+    model = ResNet("resnet152", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=16,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="ResNet152_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_resnet152_2():
+    model = ResNet("resnet152", pretrained=True)
+    train(
+        model,
+        10,
+        batch_size=16,
+        num_workers=16,
+        callbacks=ResNetFineTune(30),
+        logger_version="ResNet152_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+def train_resnet152_3():
+    train(
+        "resnet152",
+        10,
+        pretrained=True,
+        batch_size=16,
+        num_workers=16,
+        callbacks=BackboneFinetuning(30, lambda_func=lambda epoch: 0.95, verbose=True),
+        logger_version="ResNet152_mix_freeze_all_DefinedNet",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+def train_resnet152_4():
+    model = ResNet("resnet152", pretrained=True)
+    train(
+        model,
+        10,
+        batch_size=16,
+        num_workers=16,
+        callbacks=ResNetFineTune(0),
+        logger_version="ResNet152_finetune",
+        predict=True,
+        check_val_every_n_epoch=2,
+        split=False,
+        best_model=False,
+    )
+
+
+def train_resnet152_5():
+    model = ResNet("resnet152", pretrained=True)
+    train(
+        model,
+        20,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(30),
+        logger_version="ResNet152_resize96_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+        resize=96,
+    )
+
+
+def train_resnet152_6():
+    model = ResNet("resnet152", pretrained=True)
+    train(
+        model,
+        20,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 15),
+        logger_version="ResNet152_resize96_mix_freeze_5_15",
+        mix=cutmix_or_mixup,
+        predict=False,
+        resize=96,
+    )
+
+
+def train_regnety_1():
+    model = RegNet("regnet_y_3_2gf", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(5, 25),
+        logger_version="RegNetY_3.2GF_mix_freeze_5_25",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_regnety_2():
+    model = RegNet("regnet_y_3_2gf", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=16,
+        callbacks=ResNetFineTune(50),
+        logger_version="RegNetY_3.2GF_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_regnety_3():
+    model = RegNet("regnet_y_8gf", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=24,
+        num_workers=8,
+        callbacks=ResNetFineTune(50),
+        logger_version="RegNetY_8GF_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_regnety_3():
+    model = RegNet("regnet_y_16gf", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=16,
+        num_workers=8,
+        callbacks=ResNetFineTune(50),
+        logger_version="RegNetY_16GF_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_convnext_1():
+    model = ConvNeXt("convnext_tiny", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(5, 25),
+        logger_version="ConvNeXt_Tiny_freeze_5_25",
+        predict=False,
+    )
+
+
+def train_convnext_2():
+    model = ConvNeXt("convnext_tiny", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(5, 25),
+        logger_version="ConvNeXt_Tiny_mix_freeze_5_25",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_convnext_3():
+    model = ConvNeXt("convnext_tiny", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(50),
+        logger_version="ConvNeXt_Tiny_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_convnext_4():
+    model = ConvNeXt("convnext_small", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(50),
+        logger_version="ConvNeXt_Small_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_convnext_5():
+    model = ConvNeXt("convnext_small", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=24,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(5, 25, 100),
+        logger_version="ConvNeXt_Small_mix_freeze_5_25",
+        mix=cutmix_or_mixup,
+        predict=False,
+    )
+
+
+def train_convnext_6():
+    model = ConvNeXt("convnext_base", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(50),
+        logger_version="ConvNeXt_Base_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+def train_convnext_7():
+    model = ConvNeXt("convnext_base", pretrained=True)
+    train(
+        model,
+        20,
+        batch_size=16,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(5, 15, 100),
+        logger_version="ConvNeXt_Base_mix_freeze_5_15",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+def train_convnext_8():
+    model = ConvNeXt("convnext_large", pretrained=True)
+    train(
+        model,
+        30,
+        batch_size=32,
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(50),
+        logger_version="ConvNeXt_Large_mix_freeze_all",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+def train_convnext_9():
+    model = ConvNeXt("convnext_large", pretrained=True)
+    train(
+        model,
+        20,
+        batch_size=16,  ## ???
+        num_workers=8,
+        callbacks=ConvNeXtFineTune(5, 15, 100),
+        logger_version="ConvNeXt_Large_mix_freeze_5_15",
+        mix=cutmix_or_mixup,
+        predict=True,
+    )
+
+
+if __name__ == "__main__":
+    # train_resnet18_1()
+    # train_resnet18_2()
+    # train_resnet18_3()
+    # train_resnet18_4()
+    # train_resnet34_1()
+    # train_resnet50_1()
+    # train_resnet101_1()
+    # train_resnet152_1()
+    # train_resnet152_2()
+    # train_resnet152_3()
+    # train_resnet152_4()
+    # train_resnet152_5()
+    # train_resnet152_6()
+    # train_regnety_1()
+    # train_regnety_2()
+    # train_regnety_3()
+    # train_convnext_1()
+    # train_convnext_2()
+    # train_convnext_3()
+    # train_convnext_4()
+    # train_convnext_5()
+    # train_convnext_6()
+    # train_convnext_7()
+    train_convnext_8()
+    pass
