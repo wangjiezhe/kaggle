30f940b69633fa61e6a6f5b812d8ae1094c277c2
wangjiezhe <wangjiezhe@gmail.com>
2024-02-05 20:14:35 +0800

    CowBoy Outfits Detection: use `wrap_dataset_for_transforms_v2` and augmentation

--- cowboyoutfits/data.py
+++ cowboyoutfits/data.py
@@ -2,63 +2,23 @@ import os.path
 
 import lightning as L
 import torch
+from pycocotools.coco import COCO
 from torch.utils.data import DataLoader, random_split
 from torchvision import tv_tensors
-from torchvision.datasets import CocoDetection
+from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
 from torchvision.ops import box_convert
 from torchvision.transforms import v2
 from torchvision.transforms.v2 import functional as F
 
 __all__ = [
-    "train_transform",
-    "target_transform",
-    "train_transforms",
-    "CowboyDataset",
     "CowboyData",
 ]
-
-
-class CowboyDataset(CocoDetection):
-    def __init__(self, root, annFile, transform=None, target_transform=None, transforms=None):
-        super().__init__(root, annFile, transform, target_transform, transforms)
-        self.name_to_label = {info["name"]: info["id"] for info in self.coco.loadCats(self.coco.getCatIds())}
-        self.label_to_name = {info["id"]: info["name"] for info in self.coco.loadCats(self.coco.getCatIds())}
-
-
-train_transform = v2.Compose([v2.PILToTensor(), v2.ToDtype(torch.float32, scale=True)])  # type: ignore
-
-
-def target_transform(target):
-    boxes = torch.as_tensor([ann["bbox"] for ann in target], dtype=torch.float32)  # type: ignore
-    boxes = box_convert(boxes, in_fmt="xywh", out_fmt="xyxy")
-    labels = torch.as_tensor([ann["category_id"] for ann in target])  # type: ignore
-    image_id = target[0]["image_id"]
-    area = torch.as_tensor([ann["area"] for ann in target], dtype=torch.float32)  # type: ignore
-    iscrowd = torch.as_tensor([ann["iscrowd"] for ann in target], dtype=torch.uint8)  # type: ignore
-    return {"boxes": boxes, "labels": labels, "image_id": image_id, "area": area, "iscrowd": iscrowd}
-
-
-def train_transforms(image, target):
-    image = tv_tensors.Image(image)
-    image = F.to_dtype(image, scale=True)  # type: ignore
-    boxes = tv_tensors.BoundingBoxes(
-        [ann["bbox"] for ann in target],
-        format="xywh",
-        canvas_size=F.get_size(image),  # type: ignore
-    )
-    boxes = F.convert_bounding_box_format(boxes, new_format=tv_tensors.BoundingBoxFormat.XYXY)
-    labels = torch.as_tensor([ann["category_id"] for ann in target])  # type: ignore
-    image_id = target[0]["image_id"]
-    area = torch.as_tensor([ann["area"] for ann in target], dtype=torch.float32)  # type: ignore
-    iscrowd = torch.as_tensor([ann["iscrowd"] for ann in target], dtype=torch.uint8)  # type: ignore
-    target = {"boxes": boxes, "labels": labels, "image_id": image_id, "area": area, "iscrowd": iscrowd}
-    return image, target
 
 
 class CowboyData(L.LightningDataModule):
     def __init__(
         self,
-        batch_size=1,
+        batch_size=4,
         num_workers=8,
         data_dir="./data",
         split=True,
@@ -77,24 +37,62 @@ class CowboyData(L.LightningDataModule):
         self.split = split
         self.split_random_seed = split_random_seed
         self.valid_ratio = valid_ratio
+        coco = COCO(self.train_annFile)
+        self.label_to_idx = {k: idx + 1 for idx, k in enumerate(coco.cats.keys())}
+        self.label_to_name = {k: v["name"] for k, v in coco.cats.items()}
+        self.idx_to_name = {idx + 1: v["name"] for idx, (k, v) in enumerate(coco.cats.items())}
+
+    def target_transform(self, target):
+        target["labels"].apply_(lambda x: self.label_to_idx[x])
+        return target
+
+    def train_transforms(self, image, target):
+        transforms = v2.Compose(
+            [
+                v2.ToImage(),
+                v2.RandomPhotometricDistort(),
+                v2.RandomZoomOut(fill={tv_tensors.Image: (123, 117, 104), "others": 0}),
+                v2.RandomIoUCrop(),
+                v2.RandomHorizontalFlip(),
+                v2.SanitizeBoundingBoxes(),
+                v2.ToDtype(torch.float32, scale=True),  # type: ignore
+            ]
+        )
+        target = self.target_transform(target)
+
+        return transforms(image, target)
+
+    def val_transforms(self, image, target):
+        transform = v2.Compose([v2.ToImage, v2.ToDtype(torch.float32, scale=True)])  # type: ignore
+        image = transform(image)
+        target = self.target_transform(target)
+        return image, target
 
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
-            train_dataset = CowboyDataset(
-                root=self.train_images_dir,
-                annFile=self.train_annFile,
-                transforms=train_transforms,
+            train_dataset = CocoDetection(
+                self.train_images_dir, self.train_annFile, transforms=self.train_transforms
             )
+            train_dataset = wrap_dataset_for_transforms_v2(train_dataset)
+            val_dataset = CocoDetection(
+                self.train_images_dir, self.train_annFile, transforms=self.val_transforms
+            )
+            val_dataset = wrap_dataset_for_transforms_v2(val_dataset)
             if self.split:
                 assert 0.0 < self.valid_ratio < 1.0
-                self.train_data, self.val_data = random_split(
+                self.train_data, _ = random_split(
                     train_dataset,
+                    [1.0 - self.valid_ratio, self.valid_ratio],
+                    generator=torch.Generator().manual_seed(self.split_random_seed),  # type: ignore
+                )
+                _, self.val_data = random_split(
+                    val_dataset,
                     [1.0 - self.valid_ratio, self.valid_ratio],
                     generator=torch.Generator().manual_seed(self.split_random_seed),  # type: ignore
                 )
             else:
                 self.train_data = train_dataset
-                self.val_data = train_dataset
+                self.val_data = val_dataset
 
     def train_dataloader(self):
         return DataLoader(
@@ -103,6 +101,7 @@ class CowboyData(L.LightningDataModule):
             shuffle=True,
             num_workers=self.num_workers,
             pin_memory=True,
+            collate_fn=lambda batch: tuple(zip(*batch)),
         )
 
     def val_dataloader(self):
@@ -112,4 +111,5 @@ class CowboyData(L.LightningDataModule):
             shuffle=False,
             num_workers=self.num_workers,
             pin_memory=True,
+            collate_fn=lambda batch: tuple(zip(*batch)),
         )
