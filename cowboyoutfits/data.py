2e5a9934f8d82950f743487712734481f2bdffd3
wangjiezhe <wangjiezhe@gmail.com>
2024-02-08 20:35:01 +0800

    CowBoy Outfits Detection: randomly select 10 images for each category as valid_data
    Since the distribution of categories is very imbalanced, we should carefully split the data by category, to make sure we have enough sample for evaluation.

--- cowboyoutfits/data.py
+++ cowboyoutfits/data.py
@@ -1,4 +1,5 @@
 import os.path
+import random
 
 import lightning as L
 import torch
@@ -18,16 +19,18 @@ __all__ = [
 class CowboyData(L.LightningDataModule):
     def __init__(
         self,
-        batch_size=4,
+        train_batch_size=2,
+        inference_batch_size=1,
         num_workers=8,
         data_dir="./data",
         split=True,
-        split_random_seed=42,
-        valid_ratio=0.1,
+        split_random_seed=None,
+        valid_n_per_cat=10,  # randomly select 10 images for each category as valid_data
     ):
         super().__init__()
         self.save_hyperparameters()
-        self.batch_size = batch_size
+        self.train_batch_size = train_batch_size
+        self.inference_batch_size = inference_batch_size
         self.num_workers = num_workers
         self.data_dir = os.path.expanduser(data_dir)
         self.train_annFile = os.path.join(self.data_dir, "train.json")
@@ -36,11 +39,18 @@ class CowboyData(L.LightningDataModule):
         self.test_csv = os.path.join(self.data_dir, "test.csv")
         self.split = split
         self.split_random_seed = split_random_seed
-        self.valid_ratio = valid_ratio
+        self.valid_n_per_cat = valid_n_per_cat
+
         coco = COCO(self.train_annFile)
         self.label_to_idx = {k: idx + 1 for idx, k in enumerate(coco.cats.keys())}
         self.label_to_name = {k: v["name"] for k, v in coco.cats.items()}
         self.idx_to_name = {idx + 1: v["name"] for idx, (k, v) in enumerate(coco.cats.items())}
+
+        if self.split:
+            random.seed(self.split_random_seed)
+            self.validation_images_id = []
+            for category in coco.cats.keys():
+                self.validation_images_id += random.sample(coco.catToImgs[category], self.valid_n_per_cat)
 
     def target_transform(self, target):
         target["labels"].apply_(lambda x: self.label_to_idx[x])
@@ -63,7 +73,7 @@ class CowboyData(L.LightningDataModule):
         return transforms(image, target)
 
     def val_transforms(self, image, target):
-        transform = v2.Compose([v2.ToImage, v2.ToDtype(torch.float32, scale=True)])  # type: ignore
+        transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])  # type: ignore
         image = transform(image)
         target = self.target_transform(target)
         return image, target
@@ -73,41 +83,32 @@ class CowboyData(L.LightningDataModule):
             train_dataset = CocoDetection(
                 self.train_images_dir, self.train_annFile, transforms=self.train_transforms
             )
-            train_dataset = wrap_dataset_for_transforms_v2(train_dataset)
+            if self.split:
+                train_dataset.ids = [x for x in train_dataset.ids if x not in self.validation_images_id]
+            self.train_data = wrap_dataset_for_transforms_v2(train_dataset)
+
             val_dataset = CocoDetection(
                 self.train_images_dir, self.train_annFile, transforms=self.val_transforms
             )
-            val_dataset = wrap_dataset_for_transforms_v2(val_dataset)
             if self.split:
-                assert 0.0 < self.valid_ratio < 1.0
-                self.train_data, _ = random_split(
-                    train_dataset,
-                    [1.0 - self.valid_ratio, self.valid_ratio],
-                    generator=torch.Generator().manual_seed(self.split_random_seed),  # type: ignore
-                )
-                _, self.val_data = random_split(
-                    val_dataset,
-                    [1.0 - self.valid_ratio, self.valid_ratio],
-                    generator=torch.Generator().manual_seed(self.split_random_seed),  # type: ignore
-                )
-            else:
-                self.train_data = train_dataset
-                self.val_data = val_dataset
+                val_dataset.ids = self.validation_images_id
+            self.val_data = wrap_dataset_for_transforms_v2(val_dataset)
 
     def train_dataloader(self):
         return DataLoader(
             self.train_data,
-            batch_size=self.batch_size,
+            batch_size=self.train_batch_size,
             shuffle=True,
             num_workers=self.num_workers,
             pin_memory=True,
             collate_fn=lambda batch: tuple(zip(*batch)),
+            drop_last=True,
         )
 
     def val_dataloader(self):
         return DataLoader(
             self.val_data,
-            batch_size=self.batch_size,
+            batch_size=self.inference_batch_size,
             shuffle=False,
             num_workers=self.num_workers,
             pin_memory=True,
